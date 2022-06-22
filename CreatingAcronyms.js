6ce32c447f209d8d232a9a62629631548c4d2962
fonkwe <williajeni@gmail.com>
2022-06-21 23:29:54 -0500

    solving excercises

---
+++ CreatingAcronyms.js
@@ -0,0 +1,6 @@
+function first(word) {
+    return word[0];
+   }
+   var words = ["for","your","information"];
+   var acronym = words.map(first);
+   console.log(acronym.join("")); // displays "fyi"
---
+++ CreatingObject.js
@@ -0,0 +1,12 @@
+//Description:  Create a grades object that stores a 
+//set of student grades in an object.Provide a function for adding a 
+//grade and a function for displaying the student’s grade average.
+
+//Implementations:
+function createGradeObject() { 
+    this.gradeX = gradeX;
+    this.gradeY = gradeY;
+}
+function addingStudentGrade(grade) {
+    
+}
