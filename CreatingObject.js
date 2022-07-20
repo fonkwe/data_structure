20ee2943c43ce508721b9bcc21a7e65f99d765c4
fonkwe <williajeni@gmail.com>
2022-07-20 06:31:45 -0500

    Chapter 1 Exercise completed

--- CreatingObject.js
+++ CreatingObject.js
@@ -3,10 +3,30 @@
 //grade and a function for displaying the student’s grade average.
 
 //Implementations:
-function createGradeObject() { 
-    this.gradeX = gradeX;
-    this.gradeY = gradeY;
+function createGrade() { 
+    this.gradeStore = [];
+    this.addStudentGrade = addStudentGrade;
+    this.averageStudentGrade = this.averageStudentGrade;
 }
-function addingStudentGrade(grade) {
-    
+function addStudentGrade(grade) {
+    this.gradeStore.push(grade);  
 }
+function averageStudentGrade() {
+    let total = 0;
+    for(let i = 0; i < this.gradeStore.length; i++) {
+        total += this.gradeStore[i];
+    }
+    return total / this.gradeStore.length;
+}
+   
+let thisWeek = new createGrade();
+thisWeek.addStudentGrade(52);
+thisWeek.addStudentGrade(55);
+thisWeek.addStudentGrade(61);
+thisWeek.addStudentGrade(65);
+thisWeek.addStudentGrade(55);
+thisWeek.addStudentGrade(50);
+thisWeek.addStudentGrade(52);
+thisWeek.addStudentGrade(49);
+//thisWeek.averageStudentGrade();
+console.log(gradeStore);
